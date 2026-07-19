a6b9a0c033154bed48a2fad7b0e61a4a91b98809
TAHA <mtaghabadi14@gamil.com>
2026-07-19 20:56:10 +0330

    convert Bangame to Fastapi

---
+++ app.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return {
+        "status": "Bangame Bot is running 🚀"
+    }
+
+@app.post("/webhook")
+async def webhook(data: dict):
+    print(data)
+    return {"ok": True}
