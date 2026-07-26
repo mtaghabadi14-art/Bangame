1951d2ccdb32b39d53a0f3d0b43d73d0933b8e47
TAHA <mtaghabadi14@gamil.com>
2026-07-26 14:55:58 +0330

    Fix word game retry logic

--- handlers/word.py
+++ handlers/word.py
@@ -137,24 +137,15 @@ def check(chat_id, text):
     # ------------------------------
 
     if text == "🔁 بازی مجدد":
-           print(last_level)
 
-    if chat_id in last_level:
+     if chat_id in last_level:
 
-        select_level(chat_id, last_level[chat_id])
+        select_level(
+            chat_id,
+            last_level[chat_id]
+        )
 
-    else:
-
-        print("پیدا نشد")
-
-        if chat_id in last_level:
-
-            select_level(
-                chat_id,
-                last_level[chat_id]
-            )
-
-        return
+    return
 
     # ------------------------------
     # اگر بازی فعال نیست
